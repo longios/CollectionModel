03de811c6d5c9298eb0ec205ff23a664fa0cfa3b
Abe <wengzilong@liubihongdeMacBook-Pro.local>
2018-04-03 10:19:46 +0800

    collectionModel

--- CollectionModel 2/CollectionModel/AppDelegate.h
+++ CollectionModel 2/CollectionModel/AppDelegate.h
@@ -3,7 +3,7 @@
 //  CollectionModel
 //
 //  Created by Abe on 2018/3/12.
-//  Copyright © 2018年 heimavista. All rights reserved.
+//  Copyright © 2018年 heimavista. All rights reserved
 //
 
 #import <UIKit/UIKit.h>
